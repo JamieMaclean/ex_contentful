fb245a5c19ed8f40e4e616b6156e2e9f655a17dc
Jamie Maclean <jamie.maclean@gartner.com>
2022-09-30 07:42:52 +0200

    add typespecs

--- lib/content/resource/entry.ex
+++ lib/content/resource/entry.ex
@@ -1,6 +1,12 @@
 defmodule Content.Resource.Entry do
   defstruct [:entry, :sys, :metadata]
 
+  @type t :: %Content.Resource.Entry{
+    entry: struct(),
+    sys: map(),
+    metadata: map()
+  }
+
   defimpl Content.Resource do
     def base_url(_content_type, :content_management) do
       Content.ContentManagement.url() <> "/entries"
